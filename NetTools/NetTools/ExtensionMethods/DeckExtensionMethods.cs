149546b30bd761e1b77177bd3b74318282809871
Jorge Ponce Juan <es00656441@prosegur.com>
2022-03-07 18:32:21 +0100

    Removed bin & obj & added LinqExtensions Class

--- NetTools/NetTools/ExtensionMethods/DeckExtensionMethods.cs
+++ NetTools/NetTools/ExtensionMethods/DeckExtensionMethods.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
-namespace NetTools
+namespace NetTools.ExtensionMethods
 {
-    public static class ExtensionMethods
+    public static class DeckExtensionMethods
     {
 
         public static Deck<T> ToDeck<T>(this ICollection<T> source)
---
+++ NetTools/NetTools/ExtensionMethods/LinqExtensionMethods.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NetTools.ExtensionMethods
+{
+    /// <summary>
+    /// Linq Extension Methods Class
+    /// </summary>
+    public static class LinqExtensionMethods
+    {
+
+        /// <summary>
+        /// Updates a each collection member (it has to return the modified object to save it)
+        /// </summary>
+        /// <typeparam name="TSource">Collection item type</typeparam>
+        /// <param name="source">Collection itself</param>
+        /// <param name="predicate">Function to execute</param>
+        /// <returns>Modified collection</returns>
+        public static IEnumerable<TSource> Update<TSource>(this IEnumerable<TSource> source, Func<TSource, TSource> predicate)
+        {
+            if (source != null)
+            {
+                foreach (TSource item in source)
+                {
+                    predicate(item);
+                }
+            }
+
+            return source;
+        }
+
+        /// <summary>
+        /// Updates a propety for each member
+        /// </summary>
+        /// <typeparam name="TSource">Collection item type</typeparam>
+        /// <param name="source">Collection itself</param>
+        /// <param name="predicate">Action to execute</param>
+        /// <returns>Modified collection</returns>
+        public static IEnumerable<TSource> Update<TSource>(this IEnumerable<TSource> source, Action<TSource> predicate)
+        {
+            if (source != null)
+            {
+                foreach (TSource item in source)
+                {
+                    predicate(item);
+                }
+            }
+
+            return source;
+        }
+
+        /// <summary>
+        /// Consolidates a collection of collections of {TSource} into a single collection of distinct {TSource}
+        /// </summary>
+        /// <typeparam name="TSource">Collection item type</typeparam>
+        /// <param name="source">Collection itself</param>
+        /// <returns>A consolidated collection of {TSource}</returns>
+        public static IEnumerable<TSource> Consolidate<TSource>(this IEnumerable<IEnumerable<TSource>> source)
+        {
+            IEnumerable<TSource> result = null;
+            //IEnumerable<TSource> result = new List<TSource>();
+
+            foreach (var item in source)
+            {
+
+                if (result == null)
+                {
+                    result = item;
+                }
+                else
+                {
+                    result = result.Concat(item);
+                }
+            }
+
+            result = result.Distinct();
+
+            return result;
+        }
+    }
+}
